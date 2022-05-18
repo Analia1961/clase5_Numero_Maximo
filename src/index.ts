b2a0e7956aa36782e80db7160ff76f7880146361
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-18 19:47:01 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+/* Encontrar el Número Máximo
+• Leer valores hasta que se introduzca un cero (0)
+• El usuario puede introducir valores positivos y negativos
+• Encontrar el máximo de los elementos que se introdujeron
+• Analizar cómo cambia el programa para hallar el mínimo */
+
+/* Ingrese número: 9
+Ingrese número: 7
+Ingrese número: -1
+Ingrese número: 1
+Ingrese número: 0
+El máximo es 9 
+*/
+
+let conIngresos: boolean = false;
+let num = Number(prompt("Ingrese un número"));
+let maxNum: number = num;
+while (num !== 0) {
+  conIngresos = true;
+  if (num > maxNum) {
+    maxNum = num;
+  }
+  num = Number(prompt("Ingrese un número"));
+}
+if (conIngresos) {
+  console.log("El máximo es ", maxNum);
+}
+
+/*
+let conIngresos: boolean = false;
+let num = Number(prompt("Ingrese un número"));
+let minNum: number = num;
+while (num !== 0) {
+  conIngresos = true;
+  if (num < minNum) {
+  minNum = num;
+  }
+  num = Number(prompt("Ingrese un número"));
+}
+if (conIngresos) {
+console.log(“El mínimo es " , minNum);
+}*/
