89e12da9e3ab52a74fdb3a225a402cb2d9a10aec
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-18 23:31:26 -0300

    clase5_ejercicio6 se completaron títulos faltantes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-/* Encontrar el Número Máximo
+// CLASE 5 - Lunes 2 de mayo de 2022
+// Estructuras de Control - CICLOS
+
+// Ejercicio 6: Encontrar el Número Máximo
+/*
 • Leer valores hasta que se introduzca un cero (0)
 • El usuario puede introducir valores positivos y negativos
 • Encontrar el máximo de los elementos que se introdujeron
